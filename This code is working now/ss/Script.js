14fe478708155a79074ce3790ebceb7f783e7937
Aioramu <31703899+Aioramu@users.noreply.github.com>
2017-12-15 14:20:20 +0300

    Update Script.js

--- This code is working now/ss/Script.js
+++ This code is working now/ss/Script.js
@@ -1,25 +1,13 @@
 
+
 var  c = prompt("enter u name");
 alert("Hello there, "+c);
 
 window.onload = function() {
     var x = document.getElementById('demo');
     x.onclick = function() {
-         alert(c+", poshel nahui");
+         alert(c+", thi is a usualy button");
     }
 }
-window.onload = function(a){
-    var pos = 0;
-var box= document.getElementById("b");
-var t = setInterval(move,10);
-function move(){
-    if(pos>=260){
-        clearInterval(t);
-    
-    }
-    else {
-    pos +=1;
-    box.style.left = pos+"px";
-    }
-}
-}
+
+
